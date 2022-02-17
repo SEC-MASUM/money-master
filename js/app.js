fb1d59fb0c03ee380d432b4d97965a0db115dde8
SEC-MASUM <masum.jess42@gmail.com>
2022-02-17 17:56:41 +0600

    calculate button error checking done

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-console.log("Connected");
+// console.log("Connected");
 const incomeInput = document.getElementById("income-input");
 const foodInput = document.getElementById("food-cost");
 const rentInput = document.getElementById("rent-cost");
@@ -11,38 +11,72 @@ const balance = document.getElementById("balance");
 const savingAmount = document.getElementById("saving-amount");
 const remainingBalance = document.getElementById("remaining-balance");
 
+// Remaining Balance calculation function
 function balanceAmount(bigAmount, smallAmount) {
   return Math.round(
     Number.parseFloat(bigAmount) - Number.parseFloat(smallAmount)
   );
 }
-
+//Calcutale button
 calculateButton.addEventListener("click", function () {
-  console.log(incomeInput.value);
-  console.log(foodInput.value);
-  console.log(rentInput.value);
-  console.log(clothesInput.value);
+  // console.log(incomeInput.value);
+  // console.log(foodInput.value);
+  // console.log(rentInput.value);
+  // console.log(clothesInput.value);
+
+  /* Checking income and expenses fields are empty or not */
   if (
-    incomeInput.value != "" &&
-    foodInput.value != "" &&
-    rentInput.value != "" &&
+    incomeInput.value != "" ||
+    foodInput.value != "" ||
+    rentInput.value != "" ||
     clothesInput.value != ""
   ) {
+    /* Checking income and expenses fields are greater than or equal to 0 */
     if (
-      Number.parseFloat(incomeInput.value) > 0 &&
-      Number.parseFloat(foodInput.value) > 0 &&
-      Number.parseFloat(rentInput.value) > 0 &&
-      Number.parseFloat(clothesInput.value) > 0
+      Number.parseFloat(incomeInput.value) >= 0 &&
+      Number.parseFloat(foodInput.value) >= 0 &&
+      Number.parseFloat(rentInput.value) >= 0 &&
+      Number.parseFloat(clothesInput.value) >= 0
     ) {
+      // Expense calculation
       totalExpenses.innerText =
         Number.parseFloat(foodInput.value) +
         Number.parseFloat(rentInput.value) +
         Number.parseFloat(clothesInput.value);
 
+      // Remaining balance after expenses
       balance.innerText = balanceAmount(
         incomeInput.value,
         totalExpenses.innerText
       );
+    } else if (
+      Number.parseFloat(incomeInput.value) >= 0 ||
+      Number.parseFloat(foodInput.value) >= 0 ||
+      Number.parseFloat(rentInput.value) >= 0 ||
+      Number.parseFloat(clothesInput.value) >= 0
+    ) {
+      // Checking which field's value is empty or negative or string
+      if (
+        isNaN(Number.parseFloat(incomeInput.value)) ||
+        Number.parseFloat(incomeInput.value) < 0
+      ) {
+        alert("Please enter positive value in 'Income' field");
+      } else if (
+        isNaN(Number.parseFloat(foodInput.value)) ||
+        Number.parseFloat(foodInput.value) < 0
+      ) {
+        alert("Please enter positive value in 'Food' field");
+      } else if (
+        isNaN(Number.parseFloat(rentInput.value)) ||
+        Number.parseFloat(rentInput.value) < 0
+      ) {
+        alert("Please enter positive value in 'Rent' field");
+      } else if (
+        isNaN(Number.parseFloat(clothesInput.value)) ||
+        Number.parseFloat(clothesInput.value) < 0
+      ) {
+        alert("Please enter positive value in 'Clothes' field");
+      }
     } else {
       alert("Please enter positive number");
     }
@@ -51,6 +85,7 @@ calculateButton.addEventListener("click", function () {
   }
 });
 
+// Save button
 saveButton.addEventListener("click", function () {
   if (Number.parseFloat(saveInput.value) > 0) {
     savingAmount.innerText = Math.round(
