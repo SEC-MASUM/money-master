0443686a0eb0929ba0e12e16bfa92e684c141117
SEC-MASUM <masum.jess42@gmail.com>
2022-02-17 15:37:30 +0600

    error handling

--- js/app.js
+++ js/app.js
@@ -11,26 +11,56 @@ const balance = document.getElementById("balance");
 const savingAmount = document.getElementById("saving-amount");
 const remainingBalance = document.getElementById("remaining-balance");
 
+function balanceAmount(bigAmount, smallAmount) {
+  return Math.round(
+    Number.parseFloat(bigAmount) - Number.parseFloat(smallAmount)
+  );
+}
+
 calculateButton.addEventListener("click", function () {
   console.log(incomeInput.value);
   console.log(foodInput.value);
   console.log(rentInput.value);
   console.log(clothesInput.value);
+  if (
+    incomeInput.value != "" &&
+    foodInput.value != "" &&
+    rentInput.value != "" &&
+    clothesInput.value != ""
+  ) {
+    if (
+      Number.parseFloat(incomeInput.value) > 0 &&
+      Number.parseFloat(foodInput.value) > 0 &&
+      Number.parseFloat(rentInput.value) > 0 &&
+      Number.parseFloat(clothesInput.value) > 0
+    ) {
+      totalExpenses.innerText =
+        Number.parseFloat(foodInput.value) +
+        Number.parseFloat(rentInput.value) +
+        Number.parseFloat(clothesInput.value);
 
-  totalExpenses.innerText =
-    Number.parseFloat(foodInput.value) +
-    Number.parseFloat(rentInput.value) +
-    Number.parseFloat(clothesInput.value);
-
-  balance.innerText =
-    Number.parseFloat(incomeInput.value) -
-    Number.parseFloat(totalExpenses.innerText);
+      balance.innerText = balanceAmount(
+        incomeInput.value,
+        totalExpenses.innerText
+      );
+    } else {
+      alert("Please enter positive number");
+    }
+  } else {
+    alert("Please input the values to calculate");
+  }
 });
 
 saveButton.addEventListener("click", function () {
-  savingAmount.innerText =
-    Number.parseFloat(balance.innerText) *
-    (Number.parseFloat(saveInput.value) / 100);
-  remainingBalance.innerText =
-    Number.parseFloat(balance.innerText) - Number.parseFloat(savingAmount.innerText);
+  if (Number.parseFloat(saveInput.value) > 0) {
+    savingAmount.innerText = Math.round(
+      Number.parseFloat(balance.innerText) *
+        (Number.parseFloat(saveInput.value) / 100)
+    );
+
+    remainingBalance.innerText = balanceAmount(
+      balance.innerText,
+      savingAmount.innerText
+    );
+  }
 });
